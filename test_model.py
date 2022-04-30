2582ea2a01f038e46039e43f57a076477ebd838e
stacezhou <stacezhou@gmail.com>
2022-04-30 02:20:06 +0000

    [style]

--- test_model.py
+++
@@ -1,8 +0,0 @@
-from stcn import VOSMODEL
-from mmcv import Config
-import pickle
-cfg = Config.fromfile('config/model/stcn.py')
-with open('batch.pkl','rb') as fp:
-    batch = pickle.load(fp)
-model = VOSMODEL.build(cfg.model)
-model.train_step(batch,None)
