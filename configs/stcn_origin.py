63de6f58b81e32243e34ac9206f8586a7630f91e
stacezhou <stacezhou@gmail.com>
2022-05-02 13:56:14 +0000

    [exp skip15+unfrez_key] key_encoder.frozen_stages = 1

--- configs/stcn_origin.py
+++ configs/stcn_origin.py
@@ -1,16 +1,19 @@
 from configs.dataset.youtube import youtube_train,youtube_valid,youtube_debug_valid
+from configs.dataset.davis import davis_train
 from configs.model.stcn import model, custom_imports
 from configs._base.default_runtime import *
 
-youtube_train.update(dict(
+train_data_config = dict(
     max_per_frame = 2,
     max_objs_per_gpu= 8,
     frame_limit = 30,
     shuffle_videos = True,
-    random_skip = False,
+    random_skip = True,
     max_skip = 5,
     min_skip = 1,
-))
+)
+youtube_train.update(train_data_config)
+davis_train.update(train_data_config)
 
 data = dict(
     workers_per_gpu = 0,
@@ -24,16 +27,7 @@ data = dict(
 model.update(dict(
     max_per_frame = 2
 ))
-data['train'].update(dict(
-    max_per_frame = 2,
-    max_objs_per_gpu= 8,
-    frame_limit = 30,
-    shuffle_videos = True,
-    random_skip = True,
-    max_skip = 5,
-    min_skip = 3,
-))
-
+model['key_encoder']['backbone']['frozen_stages'] = 1
 optimizer = dict(type='Adam', lr=0.0005)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
