b7ed58b251f0b020c68e05aa86df2fdfbb60618e
stacezhou <stacezhou@gmail.com>
2022-05-02 15:26:35 +0000

    [fix] memory last

--- configs/stcn_origin.py
+++ configs/stcn_origin.py
@@ -24,6 +24,13 @@ data['train'].update(dict(
     min_skip = 1,
 ))
 
+model['memory'].update(dict(
+    top_k = 10,
+    mem_every = 5,
+    include_last = True,
+    thin_reading_scale = 8,
+))
+
 optimizer = dict(type='Adam', lr=0.0005)
 optimizer_config = dict(grad_clip=None)
 # learning policy
